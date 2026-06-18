7ca993cf3e23897e9883d4e6fbf3eec5e38231d2
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2026-06-17 20:41:40 -0700

    Enhance utils with new problem functions to avoid code duplication
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,7 @@ package utils
 import (
 	"net/http"
 
+	openapi "github.com/omec-project/openapi/v2"
 	"github.com/omec-project/openapi/v2/models"
 )
 
@@ -31,5 +32,44 @@ func ProblemDetailsUnspecified() *models.ProblemDetails {
 	problemDetails.SetTitle("Unspecified")
 	problemDetails.SetStatus(http.StatusForbidden)
 	problemDetails.SetCause("UNSPECIFIED")
+	return problemDetails
+}
+
+func ProblemDetailsDataNotFound() *models.ProblemDetails {
+	problemDetails := models.NewProblemDetails()
+	problemDetails.SetStatus(http.StatusNotFound)
+	problemDetails.SetCause("DATA_NOT_FOUND")
+	return problemDetails
+}
+
+func ProblemDetailsUserNotFound() *models.ProblemDetails {
+	problemDetails := models.NewProblemDetails()
+	problemDetails.SetStatus(http.StatusNotFound)
+	problemDetails.SetCause("USER_NOT_FOUND")
+	return problemDetails
+}
+
+func ProblemDetailsFromOpenAPIError(res *http.Response, err error) *models.ProblemDetails {
+	if err == nil {
+		return nil
+	}
+
+	problemDetails := ProblemDetailsSystemFailure(err.Error())
+	if res == nil || err.Error() != res.Status {
+		return problemDetails
+	}
+
+	problemDetails.SetStatus(int32(res.StatusCode))
+	if openapiErr, ok := err.(openapi.GenericOpenAPIError); ok {
+		if details, ok := openapiErr.Model().(models.ProblemDetails); ok {
+			if details.Cause != nil {
+				problemDetails.SetCause(details.GetCause())
+			}
+			if details.Status != nil {
+				problemDetails.SetStatus(details.GetStatus())
+			}
+		}
+	}
+
 	return problemDetails
 }
